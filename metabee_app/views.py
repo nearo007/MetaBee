0e03d6bc8247a114688d397d79b795e190125bbd
Nearo007 <araujo8166@gmail.com>
2025-08-08 16:49:36 -0300

    testing approaches

--- metabee_app/views.py
+++ metabee_app/views.py
@@ -3,5 +3,13 @@ from django.http import request
 
 # Create your views here.
 def index(request):
-    context = {'printer_count': (range(6))}
+    context = {'printers': [
+        {'status': True, 'name': 'Ender-3 V2'},
+        {'status': False, 'name': 'Prusa i3 MK4'},
+        {'status': False, 'name': 'Anycubic Kobra Go'},
+        {'status': False, 'name': 'Elegoo Neptune 4'},
+        {'status': True, 'name': 'Bambu Lab P1P'},
+        {'status': False, 'name': 'Voxelab Aquila X2'},
+    ]}
+
     return render(request, 'index.html', context)
