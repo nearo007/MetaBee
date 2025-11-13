08038770a980cab8372f5941e57472f6cb748452
FuturoEstagiario <envieparabruno.rodrigues@gmail.com.br>
2025-11-12 23:48:23 -0300

    funcao_api_ligada_ou_nao

---
+++ metabee_app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+# Create your models here.
+class Printer(models.Model):
+    name = models.CharField(max_length=100, default="Default Printer")
+    status = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(2)], default=0)
+    device_id = models.CharField(max_length=100, default="0")
---
+++ metabee_app/views.py
@@ -0,0 +1,106 @@
+from django.shortcuts import render, redirect
+from .utils import get_outlet_status, DEVICE_IP, LOCAL_KEY
+from django.http import request, JsonResponse
+from .models import Printer
+from .utils import get_printers_state_func, update_printers_state_in_db, DEVICE_IP, LOCAL_KEY, api_get_outlet_status
+
+# Create your views here.
+def index(request):
+    return redirect('dashboard')
+
+def dashboard(request):
+    # context = {'printers': [
+    #     {'status': 0, 'name': 'Ender-3 V2'},
+    #     {'status': 2, 'name': 'Prusa i3 MK4'},
+    #     {'status': 1, 'name': 'Anycubic Kobra Go'},
+    #     {'status': 2, 'name': 'Elegoo Neptune 4'},
+    #     {'status': 1, 'name': 'Bambu Lab P1P'},
+    #     {'status': 2, 'name': 'Voxelab Aquila X2'},
+    # ]}
+
+    printers = Printer.objects.all()
+
+    context = {'printers': printers}
+
+    return render(request, 'dashboard.html', context)
+
+def get_printers_state(request):
+    # printers = Printer.objects.all()
+    # printers_state = {}
+
+    # for printer in printers:
+    #     device_id = printer.device_id
+    #     printer_state = obter_status_tomada(device_id, DEVICE_IP, LOCAL_KEY)
+    #     #{'corrente_A': float, 'potencia_W': float, 'tensao_V': float}
+    #     printers_state[device_id] = printer_state
+
+    printers_state = get_printers_state_func()
+
+    try:
+        print(printers_state)
+        update_printers_state_in_db(printers_state)
+
+    except Exception as e:
+        print(e)
+        
+    return redirect("dashboard")
+
+def manage_printers(request):
+    printers = Printer.objects.all()
+
+    context = {'printers': printers}
+
+    return render(request, "manage_printers.html", context)
+
+def add_printer(request):
+    if request.method == 'POST':
+        printer_name = request.POST.get('name')
+        printer_status = request.POST.get('status')
+        device_id = request.POST.get('deviceId')
+        
+        try:
+            if (device_id):
+                new_printer = Printer.objects.create(name=printer_name, status=printer_status, device_id=device_id)
+            else:
+                new_printer = Printer.objects.create(name=printer_name, status=printer_status)
+
+            new_printer.save()
+            return redirect("manage_printers")
+
+        
+        except Exception as e:
+            print(e)
+
+    return render(request, "add_printer.html")
+
+def delete_printer(request, printer_id):
+    try:
+        printer = Printer.objects.filter(id=printer_id).first()
+
+        printer.delete()
+
+        return redirect("manage_printers")
+
+    except Exception as e:
+        print(e)
+        return redirect("manage_printers")
+    
+def api_get_printer_status_id(request, device_id):
+    printer = Printer.objects.filter(device_id=device_id).first()
+    printer_state = api_get_outlet_status(device_id=device_id)
+    printer_state.update({'name': printer.name})
+    return JsonResponse(printer_state, safe=False)
+
+def api_get_printer_status_name(request, printer_name):
+    printer = Printer.objects.filter(name=printer_name).first()
+    printers_state = api_get_outlet_status(device_id=printer.device_id)
+    printers_state.update({'name': printer.name})
+    return JsonResponse(printers_state, safe=False)
+
+def api_check_printer_on(request, device_id):
+    state = get_outlet_status(device_id, DEVICE_IP, LOCAL_KEY)
+    if 'erro' in state:
+        return JsonResponse({'is_active': False, 'error': state['erro']}, status=500)
+    is_active = state.get('corrente_A', 0) > 0.100
+
+    return JsonResponse({'is_active': is_active, 'device_id': device_id})
