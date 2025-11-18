5109db5acce9b857cb45df4cdf35ebd9b54f9922
Nearo007 <araujo8166@gmail.com>
2025-11-18 16:38:16 -0300

    finish updating on template

--- metabee_app/utils.py
+++ metabee_app/utils.py
@@ -4,6 +4,9 @@ from .models import Printer
 DEVICE_IP = '192.168.226.79'
 LOCAL_KEY = '96tO3A>2urrqK$#$'
 
+# # TODO for testing Webapp
+# def get_outlet_status(device_id: str, device_ip: str, local_key: str):
+#     return {'corrente_A': 0.2, 'potencia_W': 0.3, 'tensao_V': 0.4}
 def get_outlet_status(device_id: str, device_ip: str, local_key: str):
     """
     Retorna o status atual de uma tomada Tuya (corrente, potência, tensão).
@@ -41,34 +44,34 @@ def get_outlet_status(device_id: str, device_ip: str, local_key: str):
     except Exception as e:
         return {'erro': f'Falha na conexão: {e}'}
 
+# # TODO for testing API
 # def api_get_outlet_status(device_id):
-#     try:
-#         plug = tinytuya.OutletDevice(device_id, DEVICE_IP, LOCAL_KEY)
-#         plug.set_version(3.4)
-        
-#         status = plug.status()
-        
-#         if 'dps' not in status:
-#             return {'erro': 'Não foi possível obter dados DPS do dispositivo.'}
-
-#         dps = status['dps']
-#         corrente_mA = dps.get('18', 0)
-#         potencia_dW = dps.get('19', 0)
-#         tensao_dV = dps.get('20', 0)
-
-#         dados = {
-#             'corrente_A': corrente_mA / 1000.0,
-#             'potencia_W': potencia_dW / 10.0,
-#             'tensao_V': tensao_dV / 10.0
-#         }
-#         return dados
-
-#     except Exception as e:
-#         return {'erro': f'Falha na conexão: {e}'}
-
-# for testing API
+#     return {'corrente_A': 0.2, 'potencia_W': 0.3, 'tensao_V': 0.4}
 def api_get_outlet_status(device_id):
-    return {'corrente_A': 0.2, 'potencia_W': 0.3, 'tensao_V': 0.4}
+    try:
+        plug = tinytuya.OutletDevice(device_id, DEVICE_IP, LOCAL_KEY)
+        plug.set_version(3.4)
+        
+        status = plug.status()
+        
+        if 'dps' not in status:
+            return {'erro': 'Não foi possível obter dados DPS do dispositivo.'}
+
+        dps = status['dps']
+        corrente_mA = dps.get('18', 0)
+        potencia_dW = dps.get('19', 0)
+        tensao_dV = dps.get('20', 0)
+
+        dados = {
+            'corrente_A': corrente_mA / 1000.0,
+            'potencia_W': potencia_dW / 10.0,
+            'tensao_V': tensao_dV / 10.0
+        }
+        return dados
+
+    except Exception as e:
+        return {'erro': f'Falha na conexão: {e}'}
+
 
 def get_printers_state_func():
     printers = Printer.objects.all()
@@ -83,17 +86,16 @@ def get_printers_state_func():
 
 def update_printers_state_in_db(printers_state: dict):
     for device_id, state in printers_state.items():
-        printer = Printer.objects.filter(device_id=device_id)
+        printer = Printer.objects.filter(device_id=device_id).first()
         
-        if state['corrente_A'] > printer.idle and state['corrente_A'] < printer.operating_range:
+        if state['corrente_A'] >= printer.idle_range and state['corrente_A'] < printer.operating_range:
             Printer.objects.filter(device_id=device_id).update(status=1)
             
-        elif state['corrente_A'] > printer.idle and state['corrente_A']:
+        elif state['corrente_A'] >= printer.operating_range:
             Printer.objects.filter(device_id=device_id).update(status=2)
             
         else:
             Printer.objects.filter(device_id=device_id).update(status=0)
-
 
 # for device_id, state in printers_state.items():
 #     Printer.objects.filter(device_id=device_id).update(
